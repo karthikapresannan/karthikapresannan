f85d5064660d130456fc4967760d2b3fa1309d55
Karthika <karthikapresannan97@gmail.com>
2021-02-04 11:02:11 +0530

    functions

---
+++ functions/triangles.py
@@ -0,0 +1,1 @@
+num=int9input("enter the nu")
---
+++ python_collections/pythonprograms/notes.py
@@ -0,0 +1,7 @@
+for row in range(1,6):
+    for col in range (1,10):
+        if (row==5) | (row+col==6) | (row-col==4):
+            print("*",end="")
+            print("*")
+            print("*")
+            print("*",end="")
